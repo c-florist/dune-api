e1d764a9c8ccabc7279de0876f138a75949b8405
Connor Flowers <cjflowers@pm.me>
2025-09-12 09:57:00 +1000

    add missing test for get characters

--- tests/services/test_character_service.py
+++ tests/services/test_character_service.py
@@ -35,3 +35,52 @@ def test_get_character_by_uuid_returns_domain_model(monkeypatch):
     assert isinstance(result, Character)
     assert result.first_name == "Paul"
     assert result.titles == ["Duke"]
+
+
+def test_get_characters_returns_domain_models(monkeypatch):
+    characters_data = [
+        {
+            "uuid": "540b8c10-8297-4710-833e-84ef51797ac0",
+            "titles": '["Duke"]',
+            "aliases": '["The Preacher"]',
+            "first_name": "Paul",
+            "last_name": "Atreides",
+            "suffix": None,
+            "dob": "10175 AG",
+            "birthplace": "Kaitain",
+            "dod": "10219 AG",
+            "profession": '["Mentat", "Emperor"]',
+            "misc": None,
+            "house": "House Atreides",
+            "organisations": '["Bene Gesserit", "Fremen"]',
+        },
+        {
+            "uuid": "14aee0b2-2517-4d6d-a4fb-b09008d67141",
+            "titles": '["Baron"]',
+            "aliases": None,
+            "first_name": "Vladimir",
+            "last_name": "Harkonnen",
+            "suffix": None,
+            "dob": "10110 AG",
+            "birthplace": "Giedi Prime",
+            "dod": "10193 AG",
+            "profession": '["Ruler"]',
+            "misc": None,
+            "house": "House Harkonnen",
+            "organisations": "[]",
+        },
+    ]
+
+    def mock_read_characters(db_conn: Connection, house: str | None, limit: int, offset: int):
+        return characters_data
+
+    monkeypatch.setattr(queries, "read_characters", mock_read_characters)
+
+    service = CharacterService(db_conn=None)
+    result = service.get_characters(house=None, limit=20, offset=0)
+
+    assert len(result) == 2
+    assert isinstance(result[0], Character)
+    assert isinstance(result[1], Character)
+    assert result[0].first_name == "Paul"
+    assert result[1].last_name == "Harkonnen"
