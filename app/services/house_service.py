c4d97901b53e72fc7ccf9f254a30c87b81e97f08
Connor Flowers <cjflowers@pm.me>
2025-09-13 07:07:44 +1000

    add service fn for house search

--- app/services/house_service.py
+++ app/services/house_service.py
@@ -13,3 +13,7 @@ class HouseService:
     def get_houses(self, status: str | None, limit: int, offset: int) -> list[House]:
         houses_data = queries.read_houses(self.db_conn, status, limit, offset)
         return [House(**house_data) for house_data in houses_data]
+
+    def search_houses(self, search_term: str, limit: int, offset: int) -> list[House]:
+        houses_data = queries.search_houses(self.db_conn, search_term, limit, offset)
+        return [House(**house_data) for house_data in houses_data]
